0096951c9148415df95cb8a267d9e1504af58bee
chegusta <nikola.supukovic@hotmail.com>
2023-06-09 19:06:24 +0200

    inserted New rorschacchs, made array, tweaked shader

--- Scenes/ScreenSpaceFX.gd
+++ Scenes/ScreenSpaceFX.gd
@@ -4,6 +4,7 @@ extends MeshInstance3D
 var mat: Material
 var hits: int = 0
 @onready var invert_timer: Timer = $InvertTimer
+@export var tex_array: Array[Texture2D] = []
 
 func _ready():
 	EventBus.onImpact.connect(apply_effect)
@@ -12,18 +13,23 @@ func _ready():
 	invert_timer.timeout.connect(func(): mat.set_shader_parameter("invert", randi_range(0,1)))
 	invert_timer.timeout.connect(func(): mat.set_shader_parameter("offset", randf()))
 	mat = get_active_material(0)
+	var currentTexture = tex_array[0]
+	mat.set_shader_parameter("displacement_noise", currentTexture)
 
 func apply_effect():
 	mat.set_shader_parameter("impactIntensity", 1.0)
+	mat.set_shader_parameter("shake_intensity", 1.0)
 	mat.set_shader_parameter("blackout", 0.3)
 	if hits > 10:
-		mat.set_shader_parameter("stretch_multiplier", 1.0);
+		mat.set_shader_parameter("stretch_multiplier", 2.0);
 	if hits > 30:
 		mat.set_shader_parameter("abberation_value", 1.0)
 	timer.start()
 
 func reset_shader():
 	if hits < 50:
-		mat.set_shader_parameter("impactIntensity", 0.0)
-		mat.set_shader_parameter("stretch_multiplier", 0.0);
-		mat.set_shader_parameter("blackout", 0.0);
+		mat.set_shader_parameter("impactIntensity", 0.0) 
+	mat.set_shader_parameter("stretch_multiplier", 0.0);
+	mat.set_shader_parameter("blackout", 0.0);
+	mat.set_shader_parameter("shake_intensity", 0.0)
+	mat.set_shader_parameter("displacement_noise", tex_array.pick_random())
